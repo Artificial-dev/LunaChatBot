2babfe9dcda5381d1566b64441c2c2ba5fb42615
Abhishek Pawar <67840255+Artificial-dev@users.noreply.github.com>
2021-09-26 10:47:47 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+HEROKU = False # Make it False if you're not deploying on heroku.
+
+if HEROKU:
+    from os import environ
+
+    bot_token = environ["2016487075:AAHSrlYXi2I8nuEg38Kdb2AG_M_S6sJKS-w"]
+    ARQ_API_KEY = environ["GJRBXI-VJSVII-BKPOJT-LYMVEE-ARQ"]
+    LANGUAGE = environ["en"]
+
+# NOTE Fill this if you are not deploying on heroku.
+if not HEROKU:
+
+    bot_token = "2016487075:AAHSrlYXi2I8nuEg38Kdb2AG_M_S6sJKS-w"
+    ARQ_API_KEY = "GJRBXI-VJSVII-BKPOJT-LYMVEE-ARQ"
+# List of supported languages >>
+# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
+    LANGUAGE = "en"
+
+# Leave it as it is
+ARQ_API_BASE_URL = "https://thearq.tech"
